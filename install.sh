108fef48ddf56ab1f9baf8fe30cfbcedc4ccc1f7
Diego Parra <diegoparraferreira@gmail.com>
2022-06-17 17:38:52 -0400

    fix: adjust docker installation

--- install.sh
+++ install.sh
@@ -35,24 +35,30 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}
 
 function docker(){
 ### Install Docker CE
-sudo apt-get remove -y docker docker-engine docker.io
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
 sudo apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-  | sudo apt-key add - \
-  && sudo apt-key fingerprint 0EBFCD88
+sudo mkdir -p /etc/apt/keyrings
 
-sudo add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) \
-stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+newgrp docker
+
 }
 
 function pip(){
